184a5c2b5185be25d46cd64c727ca2e35822e6ec
Tristan <tristanteasdale@gmail.com>
2018-01-08 16:46:58 +0000

    Solution to Day 12 part 1

--- day12.py
+++ day12.py
@@ -14,6 +14,7 @@ def digital_plumber(programs_list):
     regex = r'(?P<prog_id>\d+) <-> (?P<prog_list>.*)'
     programs = dict()
 
+    # -Read data and populate dictionary-
     for program_str in programs_list:
         match = re.match(regex, program_str)
         prog_id = int(match.group('prog_id'))
@@ -21,12 +22,19 @@ def digital_plumber(programs_list):
 
         programs[prog_id] = prog_list
 
-    group0_programs = []
-    programs_to_check = []
+    # -Find programs in group 0-
+    group0_programs = [0]
 
-    group0_programs.append(0)
+    for p_id in group0_programs:
+        p_list = programs[p_id]
 
-    print()
+        for p in p_list:
+            if p not in group0_programs:
+                group0_programs.append(p)
+
+    group0 = set(group0_programs)
+
+    print('Num programs in group 0: ', len(group0))
 
 
 def load_puzzle():
@@ -36,7 +44,7 @@ def load_puzzle():
 
 
 if __name__ == '__main__':
-    in_data = [x for x in (TEST.strip('\n')).split('\n')]
-    # in_data = load_puzzle()
+    #in_data = [x for x in (TEST.strip('\n')).split('\n')]
+    in_data = load_puzzle()
 
     digital_plumber(in_data)
